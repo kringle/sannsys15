0c492853bf596b497b9db7167c4478854c01687d
Martin Ø. Christensen <martin.o.christensen@gmail.com>
2015-09-01 10:42:46 +0200

    working with fork

--- lab/lab2/assignmentA/main.c
+++ lab/lab2/assignmentA/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
 
+
+#define NUMBER_OF_TIMES 10
 
 int globalVar = 10;
 
@@ -9,7 +13,25 @@ int main(int argc, char **argv)
 {
 
 	int localVar = 100;
+	
+	pid_t pid = fork();
+
+	if ( pid == -1 ){
+		perror("fork failed");
+		exit(EXIT_FAILURE);
+		
+	}
+	
+	
+	int i;
+
+	for ( i = 0; i < NUMBER_OF_TIMES ; i++ ) {
+		globalVar++;
+		localVar++;
+		printf("GlobalVar = %i, localVar = %i, pid= %i \n", globalVar,localVar,pid);		
+	}
 
 	
 	return 0;
 }
+
