833a872af2c52f1e2c53b880667f9865e49f99aa
student <student@sanntid.com>
2015-11-10 13:21:43 +0100

    completed part 1, added plots

--- miniproject/part1/main.c
+++ miniproject/part1/main.c
@@ -1,30 +1,99 @@
 #include <stdio.h>
 #include "miniproject.h"
 #include <time.h>
+#include <stdlib.h>
+
+
+#define PERIOD 	0.005 // [s]
+#define RUNTIME 0.5   // [s]
+
+void setU ( float u );
+
+struct udp_conn conn;
 
 int main(int *argc, char argv[]) {
 
 
-	struct udp_conn testMsg;
-	char testBuf[100] = "Hello, AVR!";
+	
+	char sendBuf[100];
+	char receiveBuf[100];	
 	struct timespec waitTime;
 	clock_gettime(CLOCK_REALTIME, &waitTime);
+	udp_init_client(&conn, 9999, "192.168.0.1");
+
+	//VARIABLES FOR REGULATING
+	double error, integral, u, y;
+	double reference = 1;
+	double Kp = 10;
+	double Ki = 800;
+	double period = PERIOD;
+
 
 	
-
+	
+	
+	
+	strncpy(sendBuf,"START", sizeof(sendBuf));	
+	udp_send(&conn,sendBuf,sizeof(sendBuf)); //strlen(sendBuf)+1
+	
+	//strncpy(sendBuf,"SET:12.3456",sizeof(sendBuf));	
+	//udp_send(&conn,sendBuf,sizeof(sendBuf));
 
 	
-	udp_init_client(&testMsg, 9999, "192.168.0.1");
-	while(1){
+	int numRep = RUNTIME/PERIOD;
 
-		udp_send(&testMsg,testBuf,strlen(testBuf)+1);
-		printf("Hello world\n");
-		timespec_add_us(&waitTime,500000);
-		clock_nanosleep(&waitTime);		
+	int i;
+	for ( i = 0 ; i < numRep ; i++ ){
+		timespec_add_us(&waitTime,PERIOD*1000000);
+		clock_nanosleep(&waitTime);
+
+		strncpy(sendBuf,"GET", sizeof(sendBuf));
+		udp_send(&conn,sendBuf,sizeof(sendBuf));
+		udp_receive(&conn,receiveBuf,sizeof(receiveBuf));
+	
+		
+		char * numVal = receiveBuf + 8;	
+		y = atof(numVal);
+
+		//printf("Y-value from server is %f\n",y);
+
+			
+
+		//WE HAVE A Y-VALUE, DO PI REGULATING
+		error = reference - y;
+		integral = integral + (error * period);
+		u = Kp * error + Ki * integral;
+
+		setU(u);
+	
 	
 	}
+	
+
+	strncpy(sendBuf,"STOP", sizeof(sendBuf));
+	udp_send(&conn,sendBuf,sizeof(sendBuf));
+	
 	return 0;
+}
 
+void setU ( float u ){
 
+	static char send2Buf[20] = "SET:";
+	char floatArray[20];
+	snprintf(floatArray,20,"%f",u);
+	
+		
+	
+
+	int i;
+	for ( i = 4; i < 11 ; i++){
+		send2Buf[i] = floatArray[i-4];
+	} 
+
+	udp_send(&conn,send2Buf,sizeof(send2Buf));
+
+	//printf("%s\n", send2Buf); 
 
 }
+
+
